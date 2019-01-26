f230195dbf1ba617597d3cfe8d365fb7d361b080
edamamegreen <hnvance@gmail.com>
2019-01-26 14:37:49 -0800

    Display search results from input value on button click.

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
+var queryButton = document.getElementById("querybutton");
 
-var request = new XMLHttpRequest();
+queryButton.addEventListener("click", runQuery, false);
 
-request.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=harry+potter', true);
+function updateDisplay(q) {
+    var request = new XMLHttpRequest();
+    var url = 'https://www.googleapis.com/books/v1/volumes?q=' + q;
 
+    request.open('GET', url, true);
 
-request.onload = function () {
-    var data = JSON.parse(this.response);
-    var dataList = data.items;
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+        var dataList = data.items;
+        if (request.status >= 200 && request.status < 400) {
 
-    if (request.status >= 200 && request.status < 400) {
-        dataList.forEach(book => {
+            dataList.forEach(book => {
 
-            const abook = document.createElement('div');
+                const abook = document.createElement('div');
+                const title = document.createElement('p');
+                title.setAttribute('class', 'title');
+                title.textContent = book.volumeInfo.title;
+                abook.appendChild(title);
 
-            const title = document.createElement('p');
-            title.setAttribute('class', 'title');
-            title.textContent = book.volumeInfo.title;
-            abook.appendChild(title);
+                const body = document.createElement('p');
+                // TODO: when there is no description, prevent error and display default text
+                book.volumeInfo.description = book.volumeInfo.description.substring(0, 300);
+                body.textContent = `${book.volumeInfo.description}...`;
+                abook.appendChild(body);
 
-            const body = document.createElement('p');
-            book.volumeInfo.description = book.volumeInfo.description.substring(0, 300);
-            body.textContent = `${book.volumeInfo.description}...`;
-            abook.appendChild(body);
+                const base = document.getElementById('root');
+                base.appendChild(abook);
 
-            const base = document.getElementById('root');
-            base.appendChild(abook);
+                console.log(dataList);
+            });
+        } else {
+            console.log('Sorry, there\'s and error.');
+        }
 
-            console.log(dataList);
-        });
-    } else {
-        console.log('Sorry, there\'s and error.');
     }
+
+    request.send();
 }
 
-request.send();
+function runQuery(e) {
+    const query = document.getElementById('query').value;
+    updateDisplay(query);
+}
+
+updateDisplay(' ');
+
